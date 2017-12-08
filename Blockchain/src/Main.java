cd9e471319ec2256213f013ec50f85c24ff34324
Vrushal Kulkarni <vrushal.kulkarni@gmail.com>
2017-12-08 00:18:31 -0500

    a simple blockchain implementation

--- Blockchain/src/Main.java
+++ Blockchain/src/Main.java
@@ -16,13 +16,21 @@ public class Main {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] genesisTransaction={"Rohan sent me 5 bitcoins","I sent kk 2 bitcoins"};
+		String[] genesisTransaction={"John sent me 5 bitcoins","I sent Kate 2 bitcoins"};
 		Block genesisBlock=new Block(0, genesisTransaction);
 		
-		String[] block2Transactions={"I sent kancha 3 bitcoins", "Kancha sent rohan 1 bitcoin"};
+		String[] block2Transactions={"I sent Ron 3 bitcoins", "John sent rohan 1 bitcoin"};
 		Block block2=new Block(genesisBlock.getBlockHash(),block2Transactions);
+		
+		String[] block3Transactions={"kate sent Ron 1 bitcoins", "Ron sent me 1 bitcoin"};
+		Block block3=new Block(block2.getBlockHash(),block3Transactions);
+		
+		//Changes in any of the previous block leads to changes in the hash value of next block.
+		//Thus any misrepresentations with the previous data can be easily detected by matching
+		//the hash value of the most recent block across various blockchains.
 		System.out.println(genesisBlock.getBlockHash());
 		System.out.println(block2.getBlockHash());
+		System.out.println(block3.getBlockHash());
 		
 	}
 
